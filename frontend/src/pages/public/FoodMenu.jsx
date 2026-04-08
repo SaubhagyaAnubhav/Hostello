83089d4bccc8abe4778cf7a550cac16265533d90
SaubhagyaAnubhav <Saubhagya.Anubhav@adypu.edu.in>
2026-04-09 00:11:26 +0530

    Add public food menu page

---
+++ frontend/src/pages/public/FoodMenu.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FoodMenuSection from '../../components/FoodMenuSection';
+
+const FoodMenu = () => {
+    return (
+        <div className="min-h-screen bg-slate-50 pt-24 pb-12">
+            <FoodMenuSection />
+        </div>
+    );
+};
+
+export default FoodMenu;
