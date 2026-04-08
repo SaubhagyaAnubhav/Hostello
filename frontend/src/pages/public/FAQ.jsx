f2d98f655fd8c1555d4205b23c3e1951b3ccfedc
SaubhagyaAnubhav <Saubhagya.Anubhav@adypu.edu.in>
2026-04-09 00:11:10 +0530

    Add public FAQ page

---
+++ frontend/src/pages/public/FAQ.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FAQSection from '../../components/FAQSection';
+
+const FAQ = () => {
+    return (
+        <div className="min-h-screen bg-white pt-24 pb-12">
+            <FAQSection />
+        </div>
+    );
+};
+
+export default FAQ;
