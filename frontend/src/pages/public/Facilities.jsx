6b3c9782e728eccf720be3e326b23080968458f6
SaubhagyaAnubhav <Saubhagya.Anubhav@adypu.edu.in>
2026-04-09 00:11:45 +0530

    Add public facilities page

---
+++ frontend/src/pages/public/Facilities.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import FacilitiesSection from '../../components/FacilitiesSection';
+
+const Facilities = () => {
+    return (
+        <div className="min-h-screen bg-white pt-24 pb-12">
+            <FacilitiesSection />
+        </div>
+    );
+};
+
+export default Facilities;
